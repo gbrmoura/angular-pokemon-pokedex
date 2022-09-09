dfd7ecab40814f99076db5af0d551cd587b4d24d
Gabriel Moura <gmoura2@lenovo.com>
2022-09-09 14:54:30 -0300

    removing garbage code

--- src/app/services/index.ts
+++ src/app/services/index.ts
@@ -1,6 +1,3 @@
 export * from './http.service';
 export * from './pokemon.service';
-export * from './loader.service';
-export * from './interceptor.service';
-export * from './pokemon.service';
 export * from './pokemon-formatting.service';
